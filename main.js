72d5177d9191c59a3adbb21fde95e053af7fe764
James Lee <jameslee@Jamess-MBP.home>
2023-08-09 23:30:00 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let today = new Date();
+let day = today.getDay();
+let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let time = today.getHours() + ":" + today.getMinutes();
+
+console.log("today is: " + daylist[day]); 
+console.log("current time is: " + time);
